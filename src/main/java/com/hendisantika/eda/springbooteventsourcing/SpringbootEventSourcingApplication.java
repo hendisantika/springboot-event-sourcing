bf7304aa7d1a72665de9c0a59d3bc247cebf23d5
hendisantika <hendisantika@yahoo.co.id>
2019-04-24 06:45:43 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/eda/springbooteventsourcing/SpringbootEventSourcingApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.eda.springbooteventsourcing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootEventSourcingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootEventSourcingApplication.class, args);
+    }
+
+}
