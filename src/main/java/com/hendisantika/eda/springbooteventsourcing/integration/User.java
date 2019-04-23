d5ed2414e4a02b706e6589302e22ae4f0bf9558c
hendisantika <hendisantika@yahoo.co.id>
2019-04-24 06:54:57 +0700

    #5 - Add User Integration

---
+++ src/main/java/com/hendisantika/eda/springbooteventsourcing/integration/User.java
@@ -0,0 +1,93 @@
+package com.hendisantika.eda.springbooteventsourcing.integration;
+
+import java.io.Serializable;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-event-sourcing
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2019-04-24
+ * Time: 06:54
+ */
+public class User implements Serializable {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String city;
+    private String postCode;
+    private String street;
+
+
+    private String status;
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getPostCode() {
+        return postCode;
+    }
+
+    public void setPostCode(String postCode) {
+        this.postCode = postCode;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", city='" + city + '\'' +
+                ", postCode='" + postCode + '\'' +
+                ", street='" + street + '\'' +
+                '}';
+    }
+}
+
