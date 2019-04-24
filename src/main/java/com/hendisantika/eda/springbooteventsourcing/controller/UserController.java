3fecbdf42eaf4233362b5f9ab1c02ca6b485c68b
hendisantika <hendisantika@yahoo.co.id>
2019-04-24 07:11:54 +0700

    #17 - Add index method in UserController

--- src/main/java/com/hendisantika/eda/springbooteventsourcing/controller/UserController.java
+++ src/main/java/com/hendisantika/eda/springbooteventsourcing/controller/UserController.java
@@ -1,6 +1,8 @@
 package com.hendisantika.eda.springbooteventsourcing.controller;
 
+import com.hazelcast.client.config.ClientConfig;
 import com.hazelcast.core.HazelcastInstance;
+import com.hendisantika.eda.springbooteventsourcing.domain.User;
 import com.hendisantika.eda.springbooteventsourcing.event.EventStoreContainer;
 import com.hendisantika.eda.springbooteventsourcing.repository.UserRepository;
 import org.slf4j.Logger;
@@ -9,7 +11,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Collection;
 
 /**
  * Created by IntelliJ IDEA.
@@ -36,5 +42,21 @@ public class UserController {
 
     @Autowired
     HazelcastInstance hazelcastInstance;
+
+    @GetMapping
+    public String index(Model model) {
+        ClientConfig cc = new ClientConfig();
+
+        Collection userList = hazelcastInstance.getMap("userList").values();
+        Collection deactivatedUsers = hazelcastInstance.getMap("deactivatedUsers").values();
+        Collection verifiedUsers = hazelcastInstance.getMap("verifiedUsers").values();
+
+        model.addAttribute("verifiedUsers", verifiedUsers);
+        model.addAttribute("deactivatedUsers", deactivatedUsers);
+        model.addAttribute("users", userList);
+        model.addAttribute("user", new User());
+        return "index";
+    }
+
 
 }
