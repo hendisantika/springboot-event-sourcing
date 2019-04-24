04d5f26a04bb2da54e98c975c8a7bfc9e88c9391
hendisantika <hendisantika@yahoo.co.id>
2019-04-24 07:01:34 +0700

    #11 - Add UserVerifiedEvent

---
+++ src/main/java/com/hendisantika/eda/springbooteventsourcing/event/UserVerifiedEvent.java
@@ -0,0 +1,45 @@
+package com.hendisantika.eda.springbooteventsourcing.event;
+
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-event-sourcing
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2019-04-24
+ * Time: 07:01
+ */
+@JsonTypeInfo(include = JsonTypeInfo.As.WRAPPER_OBJECT, use = JsonTypeInfo.Id.NAME)
+public class UserVerifiedEvent extends DomainEvent {
+    private Long userId;
+
+    private String adminName;
+
+    public UserVerifiedEvent() {
+        super();
+    }
+
+    public UserVerifiedEvent(long eventId, Long userId, String adminName) {
+        super(eventId, userId);
+        this.userId = userId;
+        this.adminName = adminName;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getAdminName() {
+        return adminName;
+    }
+
+    public void setAdminName(String adminName) {
+        this.adminName = adminName;
+    }
+}
