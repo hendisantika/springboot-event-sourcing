d87dc238b022b9a1ba3ed8896e9b6a828960bfb0
hendisantika <hendisantika@yahoo.co.id>
2019-04-24 07:11:01 +0700

    #16 - Add required bean in UserController

--- src/main/java/com/hendisantika/eda/springbooteventsourcing/controller/UserController.java
+++ src/main/java/com/hendisantika/eda/springbooteventsourcing/controller/UserController.java
@@ -1,7 +1,13 @@
 package com.hendisantika.eda.springbooteventsourcing.controller;
 
+import com.hazelcast.core.HazelcastInstance;
+import com.hendisantika.eda.springbooteventsourcing.event.EventStoreContainer;
+import com.hendisantika.eda.springbooteventsourcing.repository.UserRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -18,5 +24,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class UserController {
     private static final Logger LOGGER = LoggerFactory.getLogger(UserController.class);
+
+    @Autowired
+    RedisTemplate<String, EventStoreContainer> redisTemplate;
+
+    @Autowired
+    StringRedisTemplate stringRedisTemplate;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    HazelcastInstance hazelcastInstance;
 
 }
