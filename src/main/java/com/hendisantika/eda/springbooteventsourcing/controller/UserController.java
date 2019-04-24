acf68bd37f6dc8c9f9c348d8515dedf5744f057b
hendisantika <hendisantika@yahoo.co.id>
2019-04-24 07:10:36 +0700

    #15 - Add UserController

---
+++ src/main/java/com/hendisantika/eda/springbooteventsourcing/controller/UserController.java
@@ -0,0 +1,22 @@
+package com.hendisantika.eda.springbooteventsourcing.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-event-sourcing
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2019-04-24
+ * Time: 07:09
+ */
+@RequestMapping("/user")
+@Controller
+public class UserController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(UserController.class);
+
+}
